622d71d0bb3067d421e32dda65dd89ee73783346
ryanwalker64 <ryan.walkerz46@gmail.com>
2022-06-12 16:59:58 +1200

    complete

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 export default function Home () {
     return  `
+        <div class="tabItem">
         <h1 class="heading">Cafe Homepage</h1>
         <p class="subtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
             labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
             dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-            deserunt mollit anim id est laborum.</p> `
+            deserunt mollit anim id est laborum.</p> 
+            </div>`
 
             // return homeHTML;
 }
